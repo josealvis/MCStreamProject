1d6bb9a18def8e303861bfe12a3e177f845ae635
jose alvis <josealvis91@hotmail.com>
2019-11-18 19:07:37 -0800

    .

--- express/services/movieDBAPI.js
+++ express/services/movieDBAPI.js
@@ -3,7 +3,7 @@ var {depureMediaName} = require('../core/helper');
 var axios  = require('axios');
 const API_KEY =  '15d2ea6d0dc1d476efbca3eba2b9bbfb';
 
-var film ='Fight.Club.10th.Anniversary.Edition.1999.1080p.BrRip.x264.YIFY.mp4';
+var film ='hustlers';
 var name = depureMediaName(film,2);
 getPoster(name);
 
@@ -16,7 +16,10 @@ function getPoster(mediaName){
    axios.get("https://api.themoviedb.org/3/search/movie?api_key="+API_KEY+"&query=" + mediaName)
             .then(function (response) {
                 // handle success
-                if (response != "Nothing found."){                
+                if (response != "Nothing found."){    
+                   var r = response.data.results[0].poster_path ;  
+                   poster_path:"http://image.tmdb.org/t/p/w500//zBhv8rsLOfpFW2M5b6wW78Uoojs.jpg"
+        
                   console.log(JSON.parse(response.data));
                        //$('#poster').html('<p>Your search found: <strong>' + json.results[0].title + '</strong></p><img src=\"http://image.tmdb.org/t/p/w500/' + json.results[0].poster_path + '\" class=\"img-responsive\" >');
                     } 
