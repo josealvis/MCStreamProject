b53e2a540a3930cb1ca67723e9bb61441b1c34d1
jose alvis <josealvis91@hotmail.com>
2019-11-26 19:23:15 -0800

    add test

--- express/repos/mediaRepo.js
+++ express/repos/mediaRepo.js
@@ -1,12 +1,12 @@
 var fs = require('fs');
 var path = require('path');
 
-const  configPath= path.join(__dirname,'../core/config.json');
+const configPath = path.join(__dirname, '../core/config.json');
 
 class mediaRepo {
 
-    constructor(cfp = null){
-      this._configPath = cfp? cfp: configPath;
+    constructor(cfp = null) {
+        this._configPath = cfp ? cfp : configPath;
     }
 
     getMediaPaths() {
@@ -15,30 +15,35 @@ class mediaRepo {
         return data.mediaPaths;
     }
 
-    addMediaPath(path,displayName ="", nsfw =0){
+    addMediaPath(path, displayName = "", nsfw = 0) {
         let rawdata = fs.readFileSync(this._configPath);
         let data = JSON.parse(rawdata);
-        data.mediaPaths.push({path, displayName, NSFW:nsfw});
+        data.mediaPaths.push({ path, displayName, NSFW: nsfw });
         fs.writeFileSync(this._configPath, JSON.stringify(data));
     }
 
-    edditMediaPath(path,displayName, nsfw){
-        let pathObj = {path,displayName,nsfw};
+    edditMediaPath(path, displayName, nsfw) {
+        let pathObj = { path, displayName, nsfw };
         let rawdata = fs.readFileSync(this._configPath);
         let data = JSON.parse(rawdata);
-        let index = data.mediaPaths.map(function(e) { return e.path; }).indexOf(path);
+        let index = data.mediaPaths.map(function (e) { return e.path; }).indexOf(path);
         data.mediaPaths.splice(index, 1, pathObj);
         fs.writeFileSync(this._configPath, JSON.stringify(data));
     }
 
-    deleteMediaPath(path){
-        let rawdata = fs.readFileSync(this._configPath);
-        let data = JSON.parse(rawdata);
-        let index = data.mediaPaths.map(function(e) { return e.path; }).indexOf(path);
-        data.mediaPaths.splice(index, 1);
-        fs.writeFileSync(this._configPath, JSON.stringify(data));
+    deleteMediaPath(path) {
+        if (path) {
+            let rawdata = fs.readFileSync(this._configPath);
+            let data = JSON.parse(rawdata);
+            let index = data.mediaPaths.map(function (e) { return e.path; }).indexOf(path);
+            data.mediaPaths.splice(index, 1);
+            fs.writeFileSync(this._configPath, JSON.stringify(data));
+        } else { 
+            console.log("path has to be specified");
+            throw "path has to be specified"
+         }
     }
 
 }
 
-module.exports =  mediaRepo;
+module.exports = mediaRepo;
