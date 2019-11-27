5c435b6f1bcf0411d6377cb1f6db0cf23b97a70a
jose alvis <josealvis91@hotmail.com>
2019-11-26 21:29:59 -0800

    add test

---
+++ express/core/helper.test.js
@@ -0,0 +1,34 @@
+var { depureMediaName } = require('./helper');
+
+describe('depureMediaName()',()=>{
+    test('Should get the first 3 words from a file name', () => {
+        let film ='rick.and.morty.s04e01.720p.hdtv.x264-w4f[eztv]';
+        let name = depureMediaName(film);
+      expect(name).toBe("rick and morty");
+    });
+    
+    test('Should get the first 2 words from a file name', () => {
+        let film ='rick.and.morty.s04e01.720p.hdtv.x264-w4f[eztv]';
+        let name = depureMediaName(film,2);
+      expect(name).toBe("rick and");
+    });
+    
+    test('Should get the first word from a file name', () => {
+        let film ='Spider-Man.Into.The.Spider-Verse.2018.1080p.WEBRip.x264-[YTS.AM]-tumbnail';
+        let name = depureMediaName(film,1);
+      expect(name).toBe("Spider");
+    });
+    
+    test('Should return just one word when there are just one word text', () => {
+        let film ='Spider';
+        let name = depureMediaName(film);
+      expect(name).toBe("Spider");
+    });
+    
+    test('Should return just two word when there are just two word text', () => {
+        let film ='Spider///*//Man';
+        let name = depureMediaName(film);
+      expect(name).toBe("Spider Man");
+    });
+
+});
--- express/repos/mediaRepo.test.js
+++ express/repos/mediaRepo.test.js
@@ -5,38 +5,40 @@ const path = require('path');
 const mockConfigPath = path.join(__dirname, '../../test/express-mock/mock-config.json');
 const mediaRepo = new rp(mockConfigPath);
 
-test('getMediaPaths() should return data', () => {
-    cleanJSON();
-    var result = mediaRepo.getMediaPaths();
-    expect(result.length > 0).toBe(true);
-});
+describe('mediaRepo', function(){
+    test('getMediaPaths() should return data', () => {
+        cleanJSON();
+        var result = mediaRepo.getMediaPaths();
+        expect(result.length > 0).toBe(true);
+    });
+    
+    test('addMediaPath() should add new path', () => {
+        cleanJSON()
+        var name = "newpath-1";
+        mediaRepo.addMediaPath(name);
+        var result = mediaRepo.getMediaPaths();
+        expect(result.some(el => el.path === name)).toBe(true);
+    });
+    
+    test('edditMediaPath() should  edit a path', () => {
+        cleanJSON()
+        var name = "C:/path";
+        mediaRepo.edditMediaPath(name, "newDisplayName", 0);
+        var result = mediaRepo.getMediaPaths();
+        var edited = result.filter(el => el.path === name)[0];
+        expect(edited.displayName).toBe("newDisplayName");
+    });
+    
+    test('deleteMediaPath() should  delete a path', () => {
+        cleanJSON()
+        var name = "C:/path";
+        mediaRepo.deleteMediaPath(name);
+        var result = mediaRepo.getMediaPaths();
+        var edited = result.filter(el => el.path === name)[0];  
+        expect(edited).toBeUndefined();
+    });
+})
 
-test('addMediaPath() should add new path', () => {
-    cleanJSON()
-    var name = "newpath-1";
-    mediaRepo.addMediaPath(name);
-    var result = mediaRepo.getMediaPaths();
-    expect(result.some(el => el.path === name)).toBe(true);
-});
-
-test('edditMediaPath() should  edit a path', () => {
-    cleanJSON()
-    var name = "C:/path";
-    mediaRepo.edditMediaPath(name, "newDisplayName", 0);
-    var result = mediaRepo.getMediaPaths();
-    var edited = result.filter(el => el.path === name)[0];
-    expect(edited.displayName).toBe("newDisplayName");
-});
-
-test('deleteMediaPath() should  delete a path', () => {
-    cleanJSON()
-    var name = "C:/path";
-    mediaRepo.deleteMediaPath(name);
-    var result = mediaRepo.getMediaPaths();
-    var edited = result.filter(el => el.path === name)[0];
-    console.log(edited);
-    expect(edited).toBe(undefined);
-});
 
 
 function cleanJSON() {
