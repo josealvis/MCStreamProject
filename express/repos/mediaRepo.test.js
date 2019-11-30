ea016726bada8ca03965d0556406a6c9153b5ae4
jose alvis <josealvis91@hotmail.com>
2019-11-30 09:39:43 -0800

    add new test

--- express/repos/mediaRepo.test.js
+++ express/repos/mediaRepo.test.js
@@ -6,37 +6,61 @@ const mockConfigPath = path.join(__dirname, '../../test/express-mock/mock-config
 const mediaRepo = new rp(mockConfigPath);
 
 describe('mediaRepo', function(){
-    test('getMediaPaths() should return data', () => {
-        cleanJSON();
-        var result = mediaRepo.getMediaPaths();
-        expect(result.length > 0).toBe(true);
-    });
+
+    describe('getMediaPaths()', () => {
+        test('should be defined', () => {
+          expect(mediaRepo.getMediaPaths).toBeDefined();
+        });
+
+        test('should return data', () => {
+            cleanJSON();
+            var result = mediaRepo.getMediaPaths();
+            expect(result.length > 0).toBe(true);
+        });
+      });
     
-    test('addMediaPath() should add new path', () => {
-        cleanJSON()
-        var name = "newpath-1";
-        mediaRepo.addMediaPath(name);
-        var result = mediaRepo.getMediaPaths();
-        expect(result.some(el => el.path === name)).toBe(true);
-    });
+      describe('addMediaPath()', () => {
+        test('should be defined', () => {
+          expect(mediaRepo.addMediaPath).toBeDefined();
+        });
+        test('should add new path', () => {
+            cleanJSON()
+            var name = "newpath-1";
+            mediaRepo.addMediaPath(name);
+            var result = mediaRepo.getMediaPaths();
+            expect(result.some(el => el.path === name)).toBe(true);
+        });
+      });
+
+      describe('edditMediaPath()', () => {
+        test('should be defined', () => {
+          expect(mediaRepo.edditMediaPath).toBeDefined();
+        });
+        test('should  edit a path', () => {
+            cleanJSON()
+            var name = "C:/path";
+            mediaRepo.edditMediaPath(name, "newDisplayName", 0);
+            var result = mediaRepo.getMediaPaths();
+            var edited = result.filter(el => el.path === name)[0];
+            expect(edited.displayName).toBe("newDisplayName");
+        });
+      });
+
     
-    test('edditMediaPath() should  edit a path', () => {
-        cleanJSON()
-        var name = "C:/path";
-        mediaRepo.edditMediaPath(name, "newDisplayName", 0);
-        var result = mediaRepo.getMediaPaths();
-        var edited = result.filter(el => el.path === name)[0];
-        expect(edited.displayName).toBe("newDisplayName");
-    });
-    
-    test('deleteMediaPath() should  delete a path', () => {
-        cleanJSON()
-        var name = "C:/path";
-        mediaRepo.deleteMediaPath(name);
-        var result = mediaRepo.getMediaPaths();
-        var edited = result.filter(el => el.path === name)[0];  
-        expect(edited).toBeUndefined();
-    });
+      describe('deleteMediaPath()', () => {
+        test('should be defined', () => {
+          expect(mediaRepo.deleteMediaPath).toBeDefined();
+        });
+        test('should  delete a path', () => {
+            cleanJSON()
+            var name = "C:/path";
+            mediaRepo.deleteMediaPath(name);
+            var result = mediaRepo.getMediaPaths();
+            var edited = result.filter(el => el.path === name)[0];  
+            expect(edited).toBeUndefined();
+        });
+      });
+
 })
 
 function cleanJSON() {
