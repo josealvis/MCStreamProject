eb532dd1475ef126d883ce13e7eff6e62e1327ca
jose alvis <josealvis91@hotmail.com>
2020-04-20 00:00:17 -0400

    Add appStates storage

--- src/helpers/storage.js
+++ src/helpers/storage.js
@@ -1,11 +1,24 @@
 export var storage = {
-    repos: [],
+        appStates:{
+            getComponentScope:function(componentName){
+                if(this[componentName]== undefined) throw "Component is not bind";
+                return this[componentName].scope;
+            }
+        },
+        repos: [],
+        getRepos: function () {
+            return JSON.parse(JSON.stringify(this.repos));
+        },
+        setRepos: function (repos) {
+            this.repos = repos;
+        },
+        //deprecated
+        setComponetScope,
+        bindComponentToStore: setComponetScope
     
-    getRepos: function(){
-        return JSON.parse(JSON.stringify(this.repos));
-    },
-    setRepos: function(repos){
-        this.repos =  repos;
-    } 
-
 };
+
+
+function setComponetScope(componentName, scope){
+    this.appStates[componentName]={scope};
+ }
